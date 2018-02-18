37ad8205ee18ff70d8a139f21732486c5639e036
Isaque Coelho <isaque.coelho12@gmail.com>
2018-02-18 17:35:35 -0300

    Feed com API

---
+++ src/providers/movie/movie.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+/*
+  Generated class for the MovieProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MovieProvider {
+
+  private basePath:string = "https://api.themoviedb.org/3/";
+  private api_key:string = "ba08e18c56c2fc61089c9fdc2a17c645";
+
+  constructor(public http: Http) {
+    console.log('Hello MovieProvider Provider');
+  }
+
+  public getLatestMovies(){
+    return this.http.get( this.basePath + "movie/popular?api_key=" + this.api_key);
+  }
+
+}
